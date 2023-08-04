f025dd1fc4e267393276d43dccf42d4879277f01
yosong.heo <ysheo@yeonmu.kr>
2023-08-04 23:46:57 +0900

    v2.20.0 agent.java,server merge

--- scouter.server/src/main/scala/scouter/server/db/TextPermWR.scala
+++ scouter.server/src/main/scala/scouter/server/db/TextPermWR.scala
@@ -1,8 +1,8 @@
 /*
- *  Copyright 2015 the original author or authors. 
+ *  Copyright 2015 the original author or authors.
  *  @https://github.com/scouter-project/scouter
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
@@ -12,11 +12,11 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
- *  limitations under the License. 
+ *  limitations under the License.
  *
  */
 
-package scouter.server.db;
+package scouter.server.db
 
 import java.io.File
 
@@ -76,6 +76,7 @@ object TextPermWR {
     val ok = queue.put(new Data(divHash, hash, text))
     if (ok == false) {
       Logger.println("S138", 10, "queue exceeded!!");
+      queue.clear();
     }
   }
 
